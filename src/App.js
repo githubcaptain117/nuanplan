92c44671c35ea591f23723ff490e356c462ca866
Akkarawin Promkun <nd117_r09_n12@hotmail.com>
2022-06-04 23:00:32 +0700

    update router

--- src/App.js
+++ src/App.js
@@ -3,26 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>
-          HELLO WORLD!
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <navbar />
+      <Switch>
+        <Route path='/' exact component={Dogs} />
+        <Route path='/cats' component={Cats} />
+        <Route path='/sheeps' component={Sheeps} />
+        <Route path='/goats' component={Goats} />
+      </Switch>
+    </Router>
   );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <h1>
+  //         HELLO WORLD!
+  //       </h1>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
